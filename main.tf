ce08147e2a9181966b765f60a563297832d5bdf8
Alex Manelis <amanelis@gmail.com>
2020-03-29 14:30:51 -0700

    Updates to terraform testing in CICD

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 locals {
-	validation = "DNS"
+  validation = "DNS"
 }
 
 provider "aws" {
-	# The AWS region in which all resources will be created
-	region = var.aws_region
+  # The AWS region in which all resources will be created
+  region = var.aws_region
 
-	# Provider version 2.X series is the latest, but has breaking changes with 1.X series.
-	version = "~> 2.6"
+  # Provider version 2.X series is the latest, but has breaking changes with 1.X series.
+  version = "~> 2.6"
 
-	# Only these AWS Account IDs may be operated on by this template
-	allowed_account_ids = [var.aws_account_id]
+  # Only these AWS Account IDs may be operated on by this template
+  allowed_account_ids = [var.aws_account_id]
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -18,29 +18,29 @@ provider "aws" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 terraform {
-# The configuration for this backend will be filled in by Terragrunt
-backend "s3" {}
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
 
-# Only allow this Terraform version. Note that if you upgrade to a newer version, Terraform won't allow you to use an
-# older version, so when you upgrade, you should upgrade everyone on your team and your CI servers all at once.
-required_version = "= 0.12.17"
+  # Only allow this Terraform version. Note that if you upgrade to a newer version, Terraform won't allow you to use an
+  # older version, so when you upgrade, you should upgrade everyone on your team and your CI servers all at once.
+  required_version = "= 0.12.17"
 }
 
 resource "aws_acm_certificate" "certificate" {
   domain_name       = var.domain_name
   validation_method = local.validation
 
-	tags = {
+  tags = {
     Name = "${var.certificate_name}"
-	}
+  }
 }
 
 resource "aws_route53_record" "certificate_validation" {
   name    = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_name
-	records = ["${aws_acm_certificate.certificate.domain_validation_options.0.resource_record_value}"]
+  records = ["${aws_acm_certificate.certificate.domain_validation_options.0.resource_record_value}"]
   type    = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_type
   zone_id = data.aws_route53_zone.zone.id
-	ttl     = 60
+  ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "certificate" {
--- variables.tf
+++ variables.tf
@@ -20,17 +20,17 @@ variable "certificate_name" {
 }
 
 variable "domain_name" {
-	description = "Primary domain name to be used for the certificate, can include wildcard here"
+  description = "Primary domain name to be used for the certificate, can include wildcard here"
   type        = string
 }
 
 variable "hosted_name" {
-	description = "AWS Route53 domain name entry, your exact domain name"
+  description = "AWS Route53 domain name entry, your exact domain name"
   type        = string
 }
 
 variable "private_zone" {
-	description = "If zone is private or public facing"
-	type 				= bool
-	default 		= false
+  description = "If zone is private or public facing"
+  type        = bool
+  default     = false
 }
