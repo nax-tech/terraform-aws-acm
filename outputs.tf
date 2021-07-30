b545df055938b901db9b81ff3e0ebba2867873b2
Eugene Konifatov <mwdn@Eugenes-MacBook-Pro.local>
2021-07-30 19:07:38 +0300

    updated output

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@ output "acm_certificate_arn" {
 
 output "acm_certificate_dns_validation_record" {
   description = "DNS record used for ACM validation"
-  value       = "${aws_route53_record.certificate_validation.name}"
+  value       = [for record in aws_route53_record.certificate_validation : record.name]
 }
